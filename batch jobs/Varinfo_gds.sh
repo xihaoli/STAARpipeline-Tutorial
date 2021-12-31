63b6b4220d9e22b98d723cda87329a6c3358697b
Zilin Li <muzizimumu@gmail.com>
2021-12-31 18:12:17 -0500

    batch jobs for FAVORannotator

---
+++ batch jobs/Varinfo_gds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J S1
+#SBATCH -p shared
+#SBATCH --time=0-144:00
+#SBATCH --array=1-22 --mem=20000
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -x holy7c16109,holy7c04508,holy2a24314,holy2a24315
+#SBATCH --mail-type=NONE
+
+module purge
+module load gcc/8.2.0-fasrc01 openmpi/3.1.1-fasrc01
+module load intel-mkl/2017.2.174-fasrc01
+module load R/3.6.1-fasrc01
+
+export R_LIBS_USER=$HOME/R-3.6.1-MKL
+echo $R_LIBS_USER
+
+/n/home05/zilinli/R-3.6.1/bin/Rscript --slave --no-restore --no-save Varinfo_gds.R ${SLURM_ARRAY_TASK_ID} > out"${SLURM_ARRAY_TASK_ID}".Rout
