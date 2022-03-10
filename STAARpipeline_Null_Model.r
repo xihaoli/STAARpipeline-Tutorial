2ee952857e5dcf3d7c279b23280496203f061706
Xihao Li <xihaoli.hsph@gmail.com>
2022-03-10 00:53:20 -0500

    STAARpipeline

--- STAARpipeline_Null_Model.r
+++ STAARpipeline_Null_Model.r
@@ -22,16 +22,17 @@ phenotype <- read.csv("/path_to_the_file/pheno.csv")
 ## (sparse) GRM file
 sgrm <- get(load("/path_to_the_file/sGRM.Rdata"))
 ## file directory for the output file 
-output_path <- "/path_to_the_output_file/" 
+output_path <- "/path_to_the_output_file/"
 ## output file name
-output_name <- "obj_STAAR_nullmodel.Rdata"
+output_name <- "obj_nullmodel.Rdata"
 
 ###########################################################
 #           Main Function 
 ###########################################################
 
 ### fit null model
-obj.nullmodel <- fit_nullmodel(LDLadj.norm~age+age2+sex+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10, data = phenotype, kins = sgrm, kins_cutoff = 0.022, id = "userId", use_sparse = TRUE, family = gaussian(link = "identity"), verbose=TRUE)
+obj_nullmodel <- fit_nullmodel(LDLadj.norm~age+age2+sex+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10,data=phenotype,
+                               kins=sgrm,kins_cutoff=0.022,id="userId",use_sparse=TRUE,family=gaussian(link="identity"),verbose=TRUE)
 
-save(obj.nullmodel,file = paste0(output_path,output_name))
+save(obj_nullmodel,file=paste0(output_path,output_name))
 
