75b87832c4b3ccdcb5dd71a405933825d43ac081
Xihao Li <xihaoli.hsph@gmail.com>
2022-03-10 00:48:50 -0500

    STAARpipeline

--- STAARpipeline_Null_Model.r
+++ STAARpipeline_Null_Model.r
@@ -1,8 +1,8 @@
-##########################################################
+###########################################################
 # fit STAAR null model
 # Xihao Li, Zilin Li
 # 11/04/2021
-##########################################################
+###########################################################
 
 rm(list=ls())
 gc()
@@ -10,7 +10,6 @@ gc()
 library(gdsfmt)
 library(SeqArray)
 library(SeqVarTools)
-library(dplyr)
 library(STAAR)
 library(STAARpipeline)
 
@@ -18,28 +17,21 @@ library(STAARpipeline)
 #           User Input
 ###########################################################
 
-## Phenotype data
-phenotype <- get(load("/n/holystore01/LABS/xlin/Lab/xihao_zilin/UKB_WES_lipids/tmp.LDL.20211014.Rdata"))
-## GRM data
-sgrm <- get(load("/n/holystore01/LABS/xlin/Lab/rdey/UKB_Analysis/FastSparseGRM/grmout/grmout_nRandomSNPs_0.sGRM.RData"))
+## Phenotype file
+phenotype <- read.csv("/path_to_the_file/pheno.csv")
+## (sparse) GRM file
+sgrm <- get(load("/path_to_the_file/sGRM.Rdata"))
 ## file directory for the output file 
-output_path <- "/n/holystore01/LABS/xlin/Lab/xihao_zilin/UKB_WES_lipids/" 
+output_path <- "/path_to_the_output_file/" 
 ## output file name
-output_name <- "obj.STAAR.UKB.LDL.20211014.Rdata"
+output_name <- "obj_STAAR_nullmodel.Rdata"
 
 ###########################################################
 #           Main Function 
 ###########################################################
 
 ### fit null model
-obj.nullmodel <- fit_nullmodel(LDLadj.norm~age+age2+sex+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10, data = phenotype, kins = sgrm, kins_cutoff = 0.022, id = "userId", use_sparse = TRUE,family = gaussian(link = "identity"), verbose=T)
+obj.nullmodel <- fit_nullmodel(LDLadj.norm~age+age2+sex+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10, data = phenotype, kins = sgrm, kins_cutoff = 0.022, id = "userId", use_sparse = TRUE, family = gaussian(link = "identity"), verbose=TRUE)
 
 save(obj.nullmodel,file = paste0(output_path,output_name))
-
-
-
-
-
-
-
 
