bd169679e4840e956b0d71818b959791faca78cf
Xihao Li <xihaoli.hsph@gmail.com>
2022-03-10 01:09:08 -0500

    STAARpipeline

--- STAARpipeline_GENESIS_Null_Model.r
+++ STAARpipeline_GENESIS_Null_Model.r
@@ -1,8 +1,8 @@
-##########################################################
+###########################################################
 # fit STAAR null model using GENESIS package
 # Xihao Li, Zilin Li
 # 11/04/2021
-##########################################################
+###########################################################
 
 rm(list=ls())
 gc()
@@ -15,15 +15,14 @@ library(STAARpipeline)
 #           User Input
 ###########################################################
 
-## Phenotype data
-phenotype <- get(load("/n/holystore01/LABS/xlin/Lab/xihao_zilin/TOPMed_LDL/LDL_20210915_group_size_30.Rdata"))
-## GRM data
-sgrm <- get(load("/n/holystore01/LABS/xlin/Lab/zilinli/TopMed/Lipid-Phenotype/pcrelate_kinshipMatrix_sparseDeg4_v2.RData"))
+## Phenotype file
+phenotype <- read.csv("/path_to_the_file/pheno.csv")
+## (sparse) GRM file
+sgrm <- get(load("/path_to_the_file/sGRM.Rdata"))
 ## file directory for the output file 
-output_path <- "/n/holystore01/LABS/xlin/Lab/xihao_zilin/TOPMed_LDL/" 
+output_path <- "/path_to_the_output_file/"
 ## output file name
-output_name <- "obj.GENESIS.STAAR.LDL.fulladj.group.size.30.20210915.Rdata"
-
+output_name <- "obj_nullmodel_GENESIS.Rdata"
 
 ###########################################################
 #           Main Function 
@@ -31,14 +30,11 @@ output_name <- "obj.GENESIS.STAAR.LDL.fulladj.group.size.30.20210915.Rdata"
 
 ### fit null model using GENESIS 
 data_GENESIS <- as(phenotype,"AnnotatedDataFrame") # Make AnnotatedDataFrame (specifically required by GENESIS)
-obj.nullmodel.GENESIS  <- fitNullModel(data_GENESIS, outcome="LDL.norm", covars=c("age", "age2", "sex", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10", "PC11", "het_resid_var_group_rolled"), cov.mat=sgrm, group.var="het_resid_var_group_rolled", AIREML.tol = 1e-4, verbose=TRUE)
+obj_nullmodel_GENESIS <- fitNullModel(data_GENESIS,outcome="LDLadj.norm",
+                                      covars=c("age","age2","sex","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10","study_race"),
+                                      cov.mat=sgrm,group.var="study_race",AIREML.tol=1e-4,verbose=TRUE)
 
 ### convert GENESIS null model to STAAR null model
-obj.nullmodel <- genesis2staar_nullmodel(obj.nullmodel.GENESIS)
-save(obj.nullmodel,file = paste0(output_path,output_name))
-
-
-
-
-
+obj_nullmodel <- genesis2staar_nullmodel(obj_nullmodel_GENESIS)
+save(obj_nullmodel,file=paste0(output_path,output_name))
 
--- STAARpipeline_Null_Model.r
+++ STAARpipeline_Null_Model.r
@@ -31,8 +31,8 @@ output_name <- "obj_nullmodel.Rdata"
 ###########################################################
 
 ### fit null model
-obj_nullmodel <- fit_nullmodel(LDLadj.norm~age+age2+sex+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10,data=phenotype,
-                               kins=sgrm,kins_cutoff=0.022,id="userId",use_sparse=TRUE,family=gaussian(link="identity"),verbose=TRUE)
+obj_nullmodel <- fit_nullmodel(LDLadj.norm~age+age2+sex+PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10+as.factor(study_race),data=phenotype,
+                               kins=sgrm,use_sparse=TRUE,kins_cutoff=0.022,id="sample.id",family=gaussian(link="identity"),verbose=TRUE)
 
 save(obj_nullmodel,file=paste0(output_path,output_name))
 
